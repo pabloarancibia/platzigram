5e7929c5097fba9db2f47a8e07bbfb5385b9136c
pabloarancibia <pabloarancibia.dw@gmail.com>
2020-07-15 23:14:08 -0300

    Hasta clase 13

--- posts/models.py
+++ posts/models.py
@@ -1,3 +1,22 @@
+#Django
 from django.db import models
 
 # Create your models here.
+
+class user(models.Model):
+    """User model"""
+
+    email = models.EmailField(unique=True)
+    password = models.CharField(max_length=100)
+
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+
+    is_admin = models.TextField(blank=True)
+
+    bio = models.TextField(blank=True)
+
+    birthdate = models.DateField(blank=True, null=True)
+
+    created = models.DateTimeField(auto_now_add=True)
+    modified = models.DateTimeField(auto_now=True)
--- posts/views.py
+++ posts/views.py
@@ -1,22 +1,38 @@
 from django.shortcuts import render
 
 # Create your views here.
-from django.http import HttpResponse
-
+#from django.http import HttpResponse
+from django.shortcuts import render
 from datetime import datetime
 posts = [
     {
-        'name': 'Mont Blac',
-        'user': 'Yesica',
-        'timestamp': datetime.now().strftime('%b %dth, %Y'),
-        'picture': 'https://picsum.photos/200/200/?image=1036',        
+        'title': 'Mont Blanc',
+        'user': {
+            'name': 'Yésica Cortés',
+            'picture': 'https://picsum.photos/60/60/?image=1027'
+        },
+        'timestamp': datetime.now().strftime('%b %dth, %Y - %H:%M hrs'),
+        'photo': 'https://picsum.photos/800/600?image=1036',
+    },
+    {
+        'title': 'Via Láctea',
+        'user': {
+            'name': 'Christian Van der Henst',
+            'picture': 'https://picsum.photos/60/60/?image=1005'
+        },
+        'timestamp': datetime.now().strftime('%b %dth, %Y - %H:%M hrs'),
+        'photo': 'https://picsum.photos/800/800/?image=903',
+    },
+    {
+        'title': 'Nuevo auditorio',
+        'user': {
+            'name': 'Uriel (thespianartist)',
+            'picture': 'https://picsum.photos/60/60/?image=883'
+        },
+        'timestamp': datetime.now().strftime('%b %dth, %Y - %H:%M hrs'),
+        'photo': 'https://picsum.photos/500/700/?image=1076',
     }
 ]
 
 def list_posts(request):
-    content = []
-    for post in posts:
-        content.append("""
-        <p>{name}</p>
-        """.format(**post))
-    return HttpResponse('br'.join(content))
+    return render(request, 'feed.html', {'posts': posts})
